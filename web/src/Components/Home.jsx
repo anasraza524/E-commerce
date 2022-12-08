b861c804947e8722af9aa218bc97603e7fae75ad
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-12-08 22:45:35 +0500

    1st commit

---
+++ web/src/Components/Home.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import axios from 'axios';
+import { useState,useEffect } from "react"
+import {Typography,Card,CardContent,
+    TextField,Button,Paper} from '@mui/material'
+    let baseUrl = ``;
+    if (window.location.href.split(":")[0] === "http") {
+        baseUrl = `http://localhost:3000`;
+    }
+    const Home = () => {
+
+        const [ProductData, setProductData] = useState(null)
+    const [prodName, setProdName] = useState('')
+    const [prodPrice, setProdPrice] = useState('')
+    const [prodDec, setProdDec] = useState('')
+   
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        axios.post(`${baseUrl}/products`, {prodName,prodDec,prodPrice})
+        const res = await  axios.post(`${baseUrl}/products`, 
+        {
+           name: prodName,
+           price:prodDec,
+           description:prodPrice,
+        });
+
+        console.log(res.data.data)  // '{"answer":42}'
+        // res.data.headers['Content-Type']; 
+
+
+
+        .catch(err => {
+            console.log("error: ", err);
+        })
+
+        console.log("I am click handler")
+        axios.get(`${baseUrl}/products`)
+            .then(response => {
+                console.log("response: ", response.data);
+
+                setProductData(response.data);
+            })
+            .catch(err => {
+                console.log("error: ", err);
+            })
+    }
+    return (
+
+
+
+    <div>
+         <form onSubmit={submitHandler}>
+<TextField 
+                sx={{pl:5,pr:5}}
+                 size="small"
+                type="text" placeholder="enter your Product name" required
+                onChange={(e) => { setProdName(e.target.value) }}>
+  </TextField>
+
+
+                <TextField 
+                sx={{pl:5,pr:5}}
+                 size="small"
+                type="text" placeholder="enter your Product Price" required
+                onChange={(e) => { setProdPrice(e.target.value) }}>
+
+                </TextField>
+                <TextField 
+                sx={{pl:5,pr:5}}
+                 size="small"
+                type="text" placeholder="enter your product Dexription" 
+                onChange={(e) => { setProdDec(e.target.value) }}>
+
+                </TextField>
+
+                {/* <TextField 
+                sx={{pl:5,pr:5}}
+                 size="small"
+                type="file"  
+                onChange={(e) => { setCityName(e.target.value) }}>
+
+                </TextField> */}
+                <Button type="submit" variant="outlined">get </Button>
+    </form></div>
+  )
+}
+
+export default Home
