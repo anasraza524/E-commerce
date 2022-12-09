ae2404b9e038b49bb067ad3f19b39a7e104af10d
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-12-09 09:34:19 +0500

    add to card done

--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { useState, useEffect } from "react"
 import {
   Typography, Card, CardContent,
-  TextField, Button, Paper
+  TextField, Button, Paper,Chip,Box,Grid,
+      CardActions,CardActionArea,Divider,CardMedia
 } from '@mui/material'
 let baseUrl = ``;
 if (window.location.href.split(":")[0] === "http") {
@@ -12,6 +13,7 @@ if (window.location.href.split(":")[0] === "http") {
 }
 const Home = () => {
 
+  const [prodImage, setProdImage]= useState('')
   const [ProductData, setProductData] = useState(null)
   const [prodName, setProdName] = useState('')
   const [prodPrice, setProdPrice] = useState('')
@@ -93,7 +95,54 @@ const Home = () => {
 
                 </TextField>  */}
         <Button type="submit" variant="outlined">get </Button>
-      </form></div>
+      </form>
+      <Divider/>
+      
+    <Paper elevation={3} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    {(!ProductData)?null:
+    ProductData?.map((eachProduct,index)=>(
+
+
+   
+    
+    <Box key={index} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+   {(prodImage==="")?null: <CardMedia
+          component="img"
+          width="200"
+          height="200"
+          image={eachProduct.ProductImage}
+          alt="green iguana"
+        />}
+      <Box sx={{ my: 3, mx: 2 }}>
+        <Grid container alignItems="center">
+          <Grid item xs>
+            <Typography gutterBottom variant="h4" component="div">
+              {eachProduct.name}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography gutterBottom variant="h6" component="div">
+              ${eachProduct.price}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Typography color="text.secondary" variant="body2">
+          {eachProduct.description}
+        </Typography>
+      </Box>
+      <Divider variant="middle" />
+      
+      <Box sx={{ 
+        display:"flex",justifyContent:"space-evenly",
+        m:1, p:1 }}>
+        <Button color='success' variant='contained'>Add to cart</Button>
+        <Button color='success' variant='contained'>Order Now</Button>
+      </Box>
+    </Box>
+     ))
+    }
+    </Paper>
+      </div>
   )
 }
 
