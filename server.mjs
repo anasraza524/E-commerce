c2eaad3b2883cf66b53c2eefb534366091e6f506
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-12-13 22:55:31 +0500

    add product added to mongodb

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import mongoose from 'mongoose'
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongodbURL = process.env.mongodbURL || "mongodb+srv://anas:12ANASraza786@cluster0.eu5uldj.mongodb.net/?retryWrites=true&w=majority"
+const mongodbURL = process.env.mongodbURL || "mongodb+srv://anas:12ANASraza786@cluster0.eu5uldj.mongodb.net/e-martdatabase?retryWrites=true&w=majority"
 // mongodb+srv://anas:12ANASraza786@cluster0.eu5uldj.mongodb.net/?retryWrites=true&w=majority
 app.use(cors());
 app.use(express.json());
@@ -24,15 +24,15 @@ let productSchema = new mongoose.Schema({
 const productModel = mongoose.model('products', productSchema);
 
 
-let addtocartSchema = new mongoose.Schema({
-    id:Number,
-    name: { type: String, required: true },
-    price: Number,
-    description: String,
-    productImage: String,
-    createdOn: { type: Date, default: Date.now }
-});
-const addtocartModel = mongoose.model('addtocarts', addtocartSchema);
+// let addtocartSchema = new mongoose.Schema({
+//     id:Number,
+//     name: { type: String, required: true },
+//     price: Number,
+//     description: String,
+//     productImage: String,
+//     createdOn: { type: Date, default: Date.now }
+// });
+// const addtocartModel = mongoose.model('addtocarts', addtocartSchema);
 
 
 app.post('/addtocart', (req, res) => {
@@ -112,20 +112,36 @@ app.post('/product', (req, res) => {
     console.log(body.description)
     // console.log('name:',body.productImage)
 
-    products.push({
-        id: `${new Date().getTime()}`,
-        // id: body.id,
+    // products.push({
+    //     id: `${new Date().getTime()}`,
+    //     // id: body.id, (Not used yet)
+    //     name: body.name,
+    //     price: body.price,
+    //     description: body.description,
+    //     productImage: body.productImage,
+    // })
+    productModel.create({
         name: body.name,
         price: body.price,
         description: body.description,
         productImage: body.productImage,
-    })
+    },
+        (err, saved) => {
+            if (!err) {
+                console.log(saved);
 
-    res.send({
-        message: "product added successfully"
-    });
-
+                res.send({
+                    message: "product added successfully"
+                });
+            } else {
+                res.status(500).send({
+                    message: "server error"
+                })
+            }
+        })
 })
+
+
 
 
 app.get('/products', (req, res) => {
