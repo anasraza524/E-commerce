ad503e40dae65a5096ddf54d600994c09b3106a5
Muhammad Anas Raza Siddiqui <85056487+anasraza524@users.noreply.github.com>
2023-01-01 03:04:28 +0500

    forgot password apies are ready

--- auth.mjs
+++ auth.mjs
@@ -1,4 +1,9 @@
-app.post("/forget-password",(req, res) => {
+
+import dotenv from './dotenv'
+import nodemailer from "nodemailer";
+dotenv.config();
+
+app.post("/forget_password",(req, res) => {
     let body = req.body;
     body.email = body.email.toLowerCase();
     if (!body.email ) { // null check - undefined, "", 0 , false, null , NaN
@@ -27,7 +32,7 @@ app.post("/forget-password",(req, res) => {
                 console.log("token: ", token);
 
                 res.cookie('Token', token, {
-                    maxAge: 1000*60,
+                    maxAge: 90,
                     httpOnly: true,
                     sameSite: 'none',
                     secure: true
@@ -161,187 +166,68 @@ app.post("/forget-password",(req, res) => {
             </body>
             </html>`,
                       };
+                      transport.sendMail(mailOptions, (error, info) => {
+            if (!error) {
+              return res.status(200).json({ message: "Email Sent" });
+            }else{
+                return res.status(400).json({ message: "Error in Sending Email " });
+            }
+            
+          });
                       
 
             }else { // user not already exist
                 console.log("userEmail not found");
-                res.status(401).send({ message: "Incorrect email" });
+                res.status(401).send({ message: "Invalid Email" });
                 return;
             }
    
         }})
    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    try {
-      if (email) {
-        const isUser = userModel.findOne({ email: email });
-        if (isUser) {
-          // generate token
-          const secretKey = isUser._id + "pleaseSubscribe";
+    });
 
-          const token = jwt.sign({ userID: isUser._id }, secretKey, {
-            expiresIn: "5m",
-          });
 
-          const link = `http://localhost:3000/user/reset/${isUser._id}/${token}`;
 
-          // email sending
-          const transport = nodemailer.createTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",
-            port: 465,
-            auth: {
-              user: process.env.EMAIL,
-              pass: process.env.EMAIL_PASSWORD,
-            },
-          });
+app.post("/forget_password/:id/:token",(req, res) => {
+let body = req.body
+const { id, token } = req.params;
+if(!body.password){
+    res.status(400).send(
+        `required fields missing, request example: 
+            {
+                "password": "12345"
+              
+            }`
+    );
+    const isUser =  userModel.findById(id);
+    stringToHash(body.password).then(hashString => {
 
-          const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: `Password Reset Request`,
-            text: `
-<!doctype html>
-<html lang="en-US">
-<head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-    <title>Reset Password Email Template</title>
-    <meta name="description" content="Reset Password Email Template.">
-    <style type="text/css">
-        a:hover {text-decoration: underline !important;}
-    </style>
-</head>
-<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
-    <!--100% body table-->
-    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
-        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-        <tr>
-            <td>
-                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
-                    align="center" cellpadding="0" cellspacing="0">
-                    
-                    <tr>
-                        <td>
-                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td style="padding:0 35px;">
-                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
-                                            requested to reset your password</h1>
-                                        <span
-                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
-                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
-                                            We cannot simply send you your old password. A unique link to reset your
-                                            password has been generated for you. To reset your password, click the
-                                            following link and follow the instructions.
-                                        </p>
-                                        <a href=${link}
-                                            style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
-                                            Password</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                            </table>
-                        </td>
-                   
-                </table>
-            </td>
-        </tr>
-    </table>
-    <!--/100% body table-->
-</body>
-</html>`,
-            html: `
-<!doctype html>
-<html lang="en-US">
-<head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-    <title>Reset Password Email Template</title>
-    <meta name="description" content="Reset Password Email Template.">
-    <style type="text/css">
-        a:hover {text-decoration: underline !important;}
-    </style>
-</head>
-<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
-    <!--100% body table-->
-    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
-        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-        <tr>
-            <td>
-                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
-                    align="center" cellpadding="0" cellspacing="0">
-                   
-                    <tr>
-                        <td>
-                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td style="padding:0 35px;">
-                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
-                                            requested to reset your password</h1>
-                                        <span
-                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
-                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
-                                            We cannot simply send you your old password. A unique link to reset your
-                                            password has been generated for you. To reset your password, click the
-                                            following link and follow the instructions.
-                                        </p>
-                                        <a href="${link}"
-                                            style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
-                                            Password</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                            </table>
-                        </td>
-                   
-                </table>
-            </td>
-        </tr>
-    </table>
-    <!--/100% body table-->
-</body>
-</html>`,
-          };
+        userModel.findByIdAndUpdate(isUser._id,{
+          
+            password: hashString
+        },
+            (err, result) => {
+                if (!err) {
+                    console.log("data saved: ", result);
+                    res.status(201).send({ message: "user is created" });
+                } else {
+                    console.log("db error: ", err);
+                    res.status(500).send({ message: "internal server error" });
+                }
+            });
+    })
+    // const isSuccess =  authModel.findByIdAndUpdate(isUser._id, {
+    //     $set: {
+    //       password: hashedPass,
+    //     },
+    //   });
+    // return;
+}
 
-          transport.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              return res.status(400).json({ message: "Error" });
-            }
-            return res.status(200).json({ message: "Email Sent" });
-          });
-        } else {
-          return res.status(400).json({ message: "Invalid Email" });
-        }
-      } else {
-        return res.status(400).json({ message: "email is required" });
-      }
-    } catch (error) {
-      return res.status(400).json({ message: error.message });
-    }}
-);
-app.post("/forget-password/:id/:token");
+
+
+}
+
+
+
+)
--- web/src/Pages/Auth/ResetPassword.jsx
+++ web/src/Pages/Auth/ResetPassword.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
 
+import React from 'react'
+import { useState,useContext } from 'react';
+import { GlobalContext } from '../../Context/Context';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { Link } from "react-router-dom";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+const theme = createTheme();
 const ResetPassword = () => {
+    let { state, dispatch } = useContext(GlobalContext);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const data = new FormData(e.currentTarget);
+      console.log({
+        email: data.get('email'),
+       
+      });
+    };
+
+    
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+        Forgot password
+        </Typography><br />
+        <Typography component="p" variant="p">
+        Type Your Email Here </Typography>
+        <Box component="form"  noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
         
-    </div>
+        
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+           Send Email
+          </Button>
+      
+        </Box>
+      </Box>
+   
+    </Container>
+  </ThemeProvider>
   )
 }
 
